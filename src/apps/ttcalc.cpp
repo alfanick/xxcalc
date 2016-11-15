ec36c2ef2a482caeced3bb9175c55e7a210f768c
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2016-11-15 21:46:35 +0100

    using readline if found

--- src/apps/ttcalc.cpp
+++ src/apps/ttcalc.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#ifdef READLINE_FOUND
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
+
 #include "calculator/tokenizer.hpp"
 #include "calculator/parser.hpp"
 #include "calculator/linear_solver.hpp"
@@ -11,8 +16,16 @@ int main(int argc, char** argv) {
   Calculator::Parser parser;
   Calculator::LinearSolver solver(tokenizer, parser);
 
+
+#ifdef READLINE_FOUND
+  char* line;
+  while ((line = ::readline(">>> "))) {
+    ::add_history(line);
+#else
   std::string line;
   while (std::getline(std::cin, line)) {
+#endif
+
     try {
       std::cout << std::string(solver.process(line)) << std::endl;
     }
